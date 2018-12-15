d7f990121b3bcda42e4d052ad0a3d34248f25120
Chhaileng Peng <chhailengpeng@gmail.com>
2018-12-15 08:17:48 +0700

    Initial Commit

---
+++ UITableViewDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UITableViewDemo
+//
+//  Created by Chhaileng Peng on 12/15/18.
+//  Copyright © 2018 Chhaileng Peng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
